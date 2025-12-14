5791f9b3a17a092921aa272d461e19d91d6e31f0
TrickyXIII <martin.henriquez.bacian@gmail.com>
2025-12-13 23:24:55 -0300

    Agregados serializers para Insumos y Pedidos

---
+++ tienda/serializers.py
@@ -0,0 +1,13 @@
+from rest_framework import serializers
+from .models import Insumo, Pedido
+
+class InsumoSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Insumo
+        fields = '__all__'
+
+class PedidoSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Pedido
+        fields = '__all__'
+        read_only_fields = ('token', 'fecha_solicitud')
